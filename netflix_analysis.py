3fcd11a718f8c4b9520df19ede92a98e548cefc1
anjali12023 <4njalisharma007@gmail.com>
2026-07-12 16:48:23 +1000

    initial commit

---
+++ netflix_analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+#Which year had the most releases?
+# Most common genres?
+# Longest movies?
+# Movies vs TV shows?
+# Countries with the most content?
+
+df = pd.read_csv("data.csv")
+print(df.to_string())
